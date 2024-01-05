58f0b98587ade59fa6e6611ef5e52372e27213ce
Shokay <148518400+SHOKinator@users.noreply.github.com>
2024-01-05 12:23:24 +0500

    Create Number_of_Laser_Beams_in_a_Bank.py

--- python3/Number_of_Laser_Beams_in_a_Bank.py
+++ python3/Number_of_Laser_Beams_in_a_Bank.py
@@ -1,15 +1,10 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        stopLoop = False
-        ansList =[]
-        for i in range(0, len(nums)):
-            for j in range(0, len(nums)):
-                if i < j and nums[i] + nums[j] == target:
-                    ansList.append(i)
-                    ansList.append(j)
-                    stopLoop = True
-                    break
-            if stopLoop:
-                break
-
-        return ansList
+    def numberOfBeams(self, bank: List[str]) -> int:
+        ans = 0
+        prev = 0
+        for i in bank:
+            res = i.count('1')
+            if res:
+                ans += prev * res
+                prev = res
+        return ans
