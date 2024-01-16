7592972cd66ea74de78bfda7b0328ff191e0ca47
Shokay <148518400+SHOKinator@users.noreply.github.com>
2024-01-16 07:35:39 +0500

    Create Majority_Element.py

--- python3/Majority_Element.py
+++ python3/Majority_Element.py
@@ -1,1 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        numsSet = list(set(nums))
 
+        for i in range(len(numsSet)):
+            count = 0
+            for j in range(len(nums)):
+                if numsSet[i] == nums[j]:
+                    count += 1
+                if count > len(nums) / 2:
+                    return numsSet[i]
